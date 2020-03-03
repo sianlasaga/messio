b0198543082d5d6318fed347d27e5c5bb17562b0
Sian Paul Lasaga <sianlasaga@kingsland.io>
2020-03-03 21:32:03 +0800

    feat(bottomsheet gesture): add gesture for closing bottonsheet

--- lib/widgets/ConversationBottomSheet.dart
+++ lib/widgets/ConversationBottomSheet.dart
@@ -20,15 +20,29 @@ class _ConversationBottomSheetState extends State<ConversationBottomSheet> {
         backgroundColor: Colors.white,
         body: ListView(
           children: <Widget>[
-            NavigationPill(),
-            Center(
-              child: Text(
-                'Messages',
-                style: Styles.textHeading,
+            GestureDetector(
+              onVerticalDragEnd: (details) {
+                print('Dragged Down');
+                if (details.primaryVelocity > 50) {
+                  Navigator.pop(context);
+                }
+              },
+              child: ListView(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                children: <Widget>[
+                  NavigationPill(),
+                  Center(
+                    child: Text(
+                      'Messages',
+                      style: Styles.textHeading,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
               ),
-            ),
-            SizedBox(
-              height: 20,
             ),
             ListView.separated(
                 shrinkWrap: true,
